7b4d6a359ef4ab4797267d0768963a17947b4de6
Chip Oglesby <chip@creativesparkcolumbia.com>
2022-03-18 13:47:01 -0400

    Update blogPostMetadata.js

--- assets/js/blogPostMetadata.js
+++ assets/js/blogPostMetadata.js
@@ -1,23 +1,23 @@
-<script type = 'javascript'>
-pageType = document.getElementsByTagName("body")[0].id.toLowerCase().trim()
-if (pageType === 'post') {
-  function wordCount(words) {
-      var count = 0
-      for (var i = 0; i < words.length; i++) {
-          count += words[i].textContent.split(" ").length
-      }
-      return count
-  }
-  wordCount = wordCount(document.getElementsByClassName("entry-content"))
-  publishTime = document.querySelectorAll('#main > article > div > footer > span.entry-date.date.published > time')[0].dateTime
-  blogTitle = document.querySelector('#main > article > div > header > h1').innerText
-  blogDetails = {
-      event: 'read_blog_post',
-      wordCount: wordCount,
-      publishTime: publishTime,
-      blogTitle: blogTitle
-  }
+<script type="application/javascript">
+	pageType = document.getElementsByTagName("body")[0].id.toLowerCase().trim()
+	if (pageType === 'post') {
+			function wordCount(words) {
+							var count = 0
+							for (var i = 0; i < words.length; i++) {
+											count += words[i].textContent.split(" ").length
+							}
+							return count
+			}
+			wordCount = wordCount(document.getElementsByClassName("entry-content"))
+			publishTime = document.querySelectorAll('#main > article > div > footer > span.entry-date.date.published > time')[0].dateTime
+			blogTitle = document.querySelector('#main > article > div > header > h1').innerText
+			blogDetails = {
+							event: 'read_blog_post',
+							wordCount: wordCount,
+							publishTime: publishTime,
+							blogTitle: blogTitle
+			}
 
-  dataLayer.push(blogDetails)
-}
-</script>
+			dataLayer.push(blogDetails)
+	}
+	</script>
